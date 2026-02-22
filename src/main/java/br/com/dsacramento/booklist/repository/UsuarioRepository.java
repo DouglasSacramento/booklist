e196e35c4e0a35f5325e69888ab3517a606e5014
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-02-22 18:51:48 -0300

    feat: implementa em UsuarioRepository o comando findByEmail e utiliza o mesmo em UsuarioService

--- src/main/java/br/com/dsacramento/booklist/repository/UsuarioRepository.java
+++ src/main/java/br/com/dsacramento/booklist/repository/UsuarioRepository.java
@@ -4,4 +4,7 @@ import br.com.dsacramento.booklist.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findByEmail(String email);
+
 }
