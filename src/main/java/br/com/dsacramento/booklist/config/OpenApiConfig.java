95d52ca910e5350c4d873e60eda432e79cc18d01
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-02-22 18:53:14 -0300

    feat: implementa o botão de login via Baerer Token na webPage

--- src/main/java/br/com/dsacramento/booklist/config/OpenApiConfig.java
+++ src/main/java/br/com/dsacramento/booklist/config/OpenApiConfig.java
@@ -2,11 +2,15 @@ package br.com.dsacramento.booklist.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    public static final String SECURITY_SCHEME_NAME = "BearerAuth";
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -14,6 +18,15 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("BookList API")
                         .version("1.0")
-                        .description("Repositório pessoal de livros"));
+                        .description("Repositório pessoal de livros"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ));
     }
 }
